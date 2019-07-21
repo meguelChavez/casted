981cc8663b439c2188fec41e43f88fff52212ca3
Meguel <chavezmeguel@gmail.com>
2019-07-21 00:33:14 -0400

    geting movie by title

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,8 +1,47 @@
 // const db = require('../models');
+const mongoose = require('mongoose')
+const mongojs = require("mongojs");
+
+const Schema = mongoose.Schema;
+
+module.exports = (app) => {
+
+    const databaseUrl = process.env.MONGODB_URI_Dev;
+    const collections = [process.env.Collection];
+
+    // Use mongojs to hook the database to the db variable
+    const db = mongojs(databaseUrl, collections);
+
+    // This makes sure that any errors are logged if mongodb runs into an issue
+    db.on("error", function (error) {
+        console.log("Database Error:", error);
+    });
 
 
-// module.exports = (app) => {
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    app.get('/api/titles', (req, res) => {
+        console.log('titles route')
+        const { searchBy, searchInput } = req.query
+        console.log(searchBy)
+        console.log(searchInput)
+        db.Titles.find({ [searchBy]: searchInput }, (err, data) => {
+            if (err) {
+                console.log(err)
+                res.json({ err: err })
+            }
+            console.log("data")
+            if (data.length > 0) {
+                res.json(data)
+            } else {
+                res.json({ msg: 'No Results Found' })
+            }
+        })
+
+    })
 
 
 
-// }
+}
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const mongoURI = process.env.MONGODB_URI || process.env.MONGODB_URI_Dev
 
 // Configure body parsing for AJAX requests
@@ -12,13 +12,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
@@ -26,14 +26,24 @@ if (process.env.NODE_ENV === "production") {
 
 
 
-
+const Schema = mongoose.Schema;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true })
-    .then(() => console.log('connected to mongo'))
+    .then(() => {
+        console.log('connected to mongo')
+        const doc = mongoose.model('Titles', new Schema({}));
+        console.log(doc)
+        doc.find({ "TitleName": "Cavalcade" }, function (err, collection) {
+            console.log('find')
+            console.log(collection)
+        });
+
+    })
     .catch(err => console.log(err));
 
 
-// require('./routes/api-routes')(app);
+require('./routes/api-routes')(app);
+
 
 app.listen(PORT, () => {
     console.log(` server listening on port ${PORT}`);
