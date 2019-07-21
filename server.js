ca608cc7fb0bd9ef32aaebb75ad39e2b8d499f26
Meguel <chavezmeguel@gmail.com>
2019-07-21 10:44:30 -0400

    wip heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+    res.sendFile(path.join(__dirname, './client/public/index.html'));
 });
 
 // app.get('*', function (req, res) {
