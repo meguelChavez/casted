3054c76ebfe8ee1770915cdf2e741f7175dd554b
Meguel <chavezmeguel@gmail.com>
2019-07-28 11:54:54 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,111 +1,22 @@
 import React, { Component } from 'react';
-import SearchBar from './components/SearchBar'
-import { Container, Row, Col, Jumbotron } from 'reactstrap';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './pages/home'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { toast } from "react-toastify"
-import axios from 'axios'
 import NavBar from './components/NavBar';
-import MovieCard from './components/MovieCard';
-import DetailModal from './components/DetailModal';
 
 class App extends Component {
 
-  state = {
-    searchOptions: ['TitleName', 'Actors'],
-    detailModal: false,
-    results: [],
-    selectedTitle: {},
-    searchInput: '',
-    searchBy: '',
-    isLoading: false
-  }
-
-  // componentDidMount() {
-  //   this.searchMovies()
-  // }
-
-  searchMovies = async () => {
-    const { searchInput } = this.state
-    const searchBy = 'TitleName'
-    const payload = {
-      params: {
-        searchBy,
-        searchInput
-      }
-    }
-
-    const res = await axios.get(`/api/titles`, payload)
-    const results = res.data
-    console.log(res)
-    if (results.searchSuccess) {
-      this.setState({ results: res.data.data, omdb: results.omdbData })
-    } else {
-      console.log(results.message)
-      this.setState({ message: results.message, results: [], omdb: results.omdbData })
-      // , () => { setTimeout(() => this.setState({ message: null }), 2000) })
-      // return toast.error(results.message)
-    }
-
-
-    // axios.get(`/api/titles`, payload).then((res) => {
-    //   console.log(res)
-    //   this.setState({ results: res.data })
-    // })
-  }
-
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  selectTitle = (selectedMovie) => {
-    this.setState({ selectedTitle: selectedMovie }, this.toggleModal)
-  }
-  toggleModal = () => {
-    const { detailModal } = this.state
-    this.setState({ detailModal: !detailModal });
-  }
-
-
   render() {
-    const { searchOptions, isLoading, searchBy, results, omdb, detailModal, selectedTitle } = this.state
     return (
-      <Container>
-        <NavBar />
-        <Row>
-          <Col sm="12" md={{ size: 8, offset: 2 }}>
-            <h1 id="searchLabel" className="align-middle">Search A Movie By Title</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="12" md={{ size: 8, offset: 2 }}>
-            <SearchBar
-              loading={isLoading}
-              disabled={true}
-              searchOptions={searchOptions}
-              searchBy={searchBy}
-              handleInputChange={this.handleInputChange}
-              handleSearch={this.searchMovies}
-            />
-
-          </Col>
-        </Row>
-        <Row>
-          <Col className="d-flex justify-content-center">
-            {(results.length > 0) ?
-              results.map((element, i) => (
-                <MovieCard key={i} results={element} omdb={omdb} toggleModal={() => { this.selectTitle(element) }} />))
-              : (this.state.searched && !isLoading) ? <MovieCard /> : ""}
-            {/* {this.state.message ?
-              <Jumbotron>{this.state.message}</Jumbotron> : null} */}
-          </Col>
-
-        </Row>
-        <DetailModal modal={detailModal} selectedTitle={selectedTitle} size="xl" toggle={this.toggleModal} />
-      </Container >
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
